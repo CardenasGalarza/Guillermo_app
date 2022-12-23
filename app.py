7bb8e69885f70bd742e5e5e3c0e02431d7babd00
CardenasGalarza <alex10estadistica@gmail.com>
2022-12-22 19:43:55 -0500

    sss

--- app.py
+++ app.py
@@ -1,2 +1,199 @@
 import streamlit as st
-st.header('yaaaaa')
+import pickle
+from pathlib import Path
+import pandas as pd
+import numpy as np
+#from soupsieve import select  # pip install pandas openpyxl
+import streamlit_authenticator as stauth  # pip install streamlit-authenticator
+############################################ OCULTAR INFROMACION NO IMPORTANTE
+import base64
+#import pyodbc
+############################################ OCULTAR INFROMACION NO IMPORTANTE
+import warnings
+warnings.filterwarnings('ignore')
+#########################################3333
+##########################
+from datetime import datetime
+import gspread
+#import pyautogui
+
+st.set_page_config(page_title='bdtickets-Averias', page_icon="🌀", layout='centered', initial_sidebar_state='auto')
+
+## borrar nombres de la pagina
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+## fondo total
+def add_bg_from_url():
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("https://cdn.pixabay.com/photo/2018/11/13/18/06/mail-3813618_960_720.jpg 1x, https://cdn.pixabay.com/photo/2018/11/13/18/06/mail-3813618_1280.jpg");
+            background-attachment: fixed;
+            background-size: cover
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+add_bg_from_url()
+#st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+# --- USER AUTHENTICATION ---
+names = ['Giancarlos Cardenas', 'Genesis Medrano', 'Luis Llerena', 'DIANA BERNEDO', 'VIVIAN CERVERA', 'CAROL CHUNGA', 'LAURA VIERA', 'MERCEDES RAYMUNDO', 'MONTES CABANILLAS', 'RENZO RIMARACHIN', 'LORENA BENAVIDES', 'NANCY YEREN', 'GIULIANA BELLIDO', 'CARMEN HUAMANCHUMO', 'GABRIEL SANTA ANA', 'CARMEN POMA REYES', 'JOSE ECHEVARRIA', 'YORMAN MORI', 'ENZO PAULINO', 'GUSTAVO SALCEDO', 'KAREN MAYORCA', 'LESLIE PRUDENCIO', 'BARBARA HUAMANCHUMO', 'Jose Ricardo', 'Eber Hinostroza', 'Bot cardenas']
+usernames = ['Cardenas', 'Genesis', 'LLLERENAL', 'BERNEDO', 'CERVERA', 'CHUNGA', 'VIERA', 'RAYMUNDO', 'CABANILLAS', 'RIMARACHIN', 'BENAVIDES', 'YEREN', 'BELLIDO', 'ANDREA', 'SANTA ANA', 'POMA REYES', 'ECHEVARRIA', 'MORI', 'PAULINO', 'SALCEDO', 'MAYORCA', 'PRUDENCIO', 'HUAMANCHUMO', 'Argomedo', 'Hinostroza', 'Bot']
+
+# load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "sales_dashboard", "abcdef", cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+#print(username)
+#### fondo al costado
+def sidebar_bg(side_bg):
+   side_bg_ext = 'jpg'
+   st.markdown(
+      f"""
+      <style>
+      [data-testid="stSidebar"] > div:first-child {{
+          background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()});
+      }}
+      </style>
+      """,
+      unsafe_allow_html=True,
+      )
+side_bg = 'nooa.jpg'
+sidebar_bg(side_bg)
+
+
+st.markdown(
+    """
+    <style>
+
+    header .css-1595djx e8zbici2{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    }
+
+    header .logo-text{
+        margin: 0;
+        padding: 10px 26px;
+        font-weight: bold;
+        color: rgb(60, 255, 0);
+        font-size: 0.8em;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+# para los botones horizontal
+st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+
+
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+        ## borrar nombres de la pagina
+    hide_streamlit_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+
+        ## borrar nombres de la pagina
+    hide_streamlit_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
+    st.markdown(
+        """
+        <style>
+
+        header .css-1595djx e8zbici2{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        }
+
+        header .logo-text{
+            margin: 0;
+            padding: 10px 26px;
+            font-weight: bold;
+            color: rgb(60, 255, 0);
+            font-size: 0.8em;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+
+    # para los botones horizontal
+    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+
+
+
+    st.markdown(
+        f"""
+        <header class="css-1595djx e8zbici2">
+            <p class="logo-text">App Alarmas 👨🏻‍💻Giancarlos .C</p>
+        </header>
+        """,
+        unsafe_allow_html=True
+    )
+
+    def hide_anchor_link():
+        st.markdown("""
+            <style>
+            .css-15zrgzn {display: none}
+            .css-eczf16 {display: none}
+            .css-jn99sy {display: none}
+            </style>
+            """, unsafe_allow_html=True)
+    texto  = ('🔒Estamos mejorando la privacidad de la información, si aún no cuentas con tus credenciales, comunicarte con:')
+    st.caption( f'<h6 style="color:#FFFFFF;">{texto}</h6>', unsafe_allow_html=True )
+
+    textoo = ('\n\n👨🏻‍💻Luis Llerena. \n\n👨🏻‍💻Giancarlos Cardenas.')
+    st.caption( f'<h6 style="color:#FFFFFF;">{textoo}</h6>', unsafe_allow_html=True )
+    ###
+    ####
+    ####
+    ####
+    ######
+
+
+
+if authentication_status:
+    # ---- SIDEBAR ----
+    authenticator.logout("Logout", "sidebar")
+    st.sidebar.title(f"Bienvenid@ {name}")
+    #### fondo al costado
+
+    st.markdown(f'<p class="big-font"; style="text-align:center;background-image: linear-gradient(to right,white, white);color:navy;font-size:24px;border-radius:2%;"><b>CARGA TUS DATOS</b></p>', unsafe_allow_html=True)
+
+    st.sidebar.subheader("Cargar datos de acuerdo a lo requerido")
+
